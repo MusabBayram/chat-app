41f44d2b1e89a74cc785efa152a296e32a2c5ba8
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-04-25 18:36:23 +0300

    chat is done

--- public/chat.js
+++ public/chat.js
@@ -5,3 +5,24 @@ const message = document.getElementById('message')
 const submitBtn = document.getElementById('submitBtn')
 const output = document.getElementById('output')
 const feedback = document.getElementById('feedback')
+
+submitBtn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        sender: sender.value,
+    })
+})
+
+socket.on('chat', data => {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>'+ data.sender + ' : </strong>' + data.message + '</p>'
+    message.value = '';
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', sender.value)
+})
+
+socket.on('typing', data => {
+    feedback.innerHTML = '<p>' + data + ' yaziyor...</p>'
+})
--- server.js
+++ server.js
@@ -10,4 +10,13 @@ const io = socket(server)
 
 io.on('connection', (socket) => {
     console.log(socket.id);
+
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data)
+    })
 })
+
