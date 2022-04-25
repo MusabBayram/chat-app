6ff50d779704dc7dccb7af7abaf26ba704639a8a
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-04-25 14:19:00 +0300

    server is up

---
+++ public/chat.js
@@ -0,0 +1,7 @@
+const socket = io.connect('http://localhost:3000/')
+
+const sender = document.getElementById('sender')
+const message = document.getElementById('message')
+const submitBtn = document.getElementById('submitBtn')
+const output = document.getElementById('output')
+const feedback = document.getElementById('feedback')
